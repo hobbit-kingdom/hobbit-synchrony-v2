fe53c066bf5755e2b7c8bba774a95bf6d571cbf2
KLFzx <kliffa7@gmail.com>
2024-11-12 13:30:09 +0200

    add ability to read ip from file if exists

--- Hobbit Multiplayer/ConnectedPlayer.h
+++ Hobbit Multiplayer/ConnectedPlayer.h
@@ -24,147 +24,147 @@
 
 // Player classes
 class ConnectedPlayer {
-    HobbitProcessAnalyzer* hobbitProcessAnalyzer;
-    uint32_t animation;
-    float animFrame, lastAnimFrame;
-    Vector3 position, rotation;
+	HobbitProcessAnalyzer* hobbitProcessAnalyzer;
+	uint32_t animation;
+	float animFrame, lastAnimFrame;
+	Vector3 position, rotation;
 
-    uint32_t level;
-    uint32_t levels;
-    int8_t weapon;
+	uint32_t level;
+	uint32_t levels;
+	int8_t weapon;
 
-    std::queue<std::pair<uint64_t, float>> hurtEnemies;// pair <guid, Health>
+	std::queue<std::pair<uint64_t, float>> hurtEnemies;// pair <guid, Health>
 
-    LogOption::Ptr logOption_;
+	LogOption::Ptr logOption_;
 
 public:
-    ConnectedPlayer() : logOption_(LogManager::Instance().CreateLogOption("CONNECTED PLAYER"))
-    {
+	ConnectedPlayer() : logOption_(LogManager::Instance().CreateLogOption("CONNECTED PLAYER"))
+	{
 
-    }
-    void setHobbitProcessAnalyzer(HobbitGameManager& initialHobbitGameManager)
-    {
-        hobbitProcessAnalyzer = initialHobbitGameManager.getHobbitProcessAnalyzer();
-        npc.setHobbitProcessAnalyzer(hobbitProcessAnalyzer);
-    }
-    int id = -1;
-    NPC npc;
+	}
+	void setHobbitProcessAnalyzer(HobbitGameManager& initialHobbitGameManager)
+	{
+		hobbitProcessAnalyzer = initialHobbitGameManager.getHobbitProcessAnalyzer();
+		npc.setHobbitProcessAnalyzer(hobbitProcessAnalyzer);
+	}
+	int id = -1;
+	NPC npc;
 
-    void readConectedPlayerSnap(std::queue<uint8_t>& gameData) {
-        animation = convertQueueToType<uint32_t>(gameData);
-        animFrame = convertQueueToType<float>(gameData);
-        lastAnimFrame = convertQueueToType<float>(gameData);
-        position.x = convertQueueToType<float>(gameData);
-        position.y = convertQueueToType<float>(gameData);
-        position.z = convertQueueToType<float>(gameData);
-        rotation.y = convertQueueToType<float>(gameData);
-        weapon = convertQueueToType<int8_t>(gameData);
-    }
-    void readProcessEnemiesHealth(std::queue<uint8_t>& gameData) {
-        uint32_t numberHurtEnemies = convertQueueToType<uint32_t>(gameData);
-
-
-        for (int i = 0; i < numberHurtEnemies; ++i)
-        {
-            uint64_t guid = convertQueueToType<uint64_t>(gameData);
-            float health = convertQueueToType<float>(gameData);
-            hurtEnemies.push(std::pair(guid, health));
-        }
-    }
-    void readConectedPlayerLevel(std::queue<uint8_t>& gameData)
-    {
-        level = convertQueueToType<uint32_t>(gameData);
-        gameData.pop();
-    }
-
-    void processPlayer(uint8_t myId)
-    {
-        if (id == -1 || id == myId)
-            return;
-
-        // Display the data
-        logOption_->LogMessage(LogLevel::Log_Debug, "Process Msg");
-        logOption_->increaseDepth();
-        logOption_->LogMessage(LogLevel::Log_Debug, "Anim", animation);
-        logOption_->LogMessage(LogLevel::Log_Debug, "Pos", position.x, position.y, position.z);
-        logOption_->LogMessage(LogLevel::Log_Debug, "RptY", rotation.y);
-        logOption_->LogMessage(LogLevel::Log_Debug, "Weapon", int(weapon));
-        logOption_->decreaseDepth();
-
-        //set position, rotation, and animation
-        npc.setPositionX(position.x);
-        npc.setPositionY(position.y);
-        npc.setPositionZ(position.z);
-        npc.setRotationY(rotation.y);
-        if (animation != npc.getAnimation())
-        {
-            npc.setAnimation(animation);
-            npc.setAnimFrames(animFrame, lastAnimFrame);
-        }
+	void readConectedPlayerSnap(std::queue<uint8_t>& gameData) {
+		animation = convertQueueToType<uint32_t>(gameData);
+		animFrame = convertQueueToType<float>(gameData);
+		lastAnimFrame = convertQueueToType<float>(gameData);
+		position.x = convertQueueToType<float>(gameData);
+		position.y = convertQueueToType<float>(gameData);
+		position.z = convertQueueToType<float>(gameData);
+		rotation.y = convertQueueToType<float>(gameData);
+		weapon = convertQueueToType<int8_t>(gameData);
+	}
+	void readProcessEnemiesHealth(std::queue<uint8_t>& gameData) {
+		uint32_t numberHurtEnemies = convertQueueToType<uint32_t>(gameData);
 
 
-        uint64_t guidNone = 0x0;
-        if (weapon != -1)
-        {
-            //NONE
-            if (weapon == 2)
-                guidNone = 0x0D8AD910E885100D;
-            //STING
-            else if (weapon == 0)
-                guidNone = 0x0D8AD910E885100B;
-            //STAFF
-            else if (weapon == 1)
-                guidNone = 0x0D8AD910E885100A;
-            //STONE
-            else if (weapon == 3)
-                guidNone = 0x0D8AD910E885100C;
+		for (int i = 0; i < numberHurtEnemies; ++i)
+		{
+			uint64_t guid = convertQueueToType<uint64_t>(gameData);
+			float health = convertQueueToType<float>(gameData);
+			hurtEnemies.push(std::pair(guid, health));
+		}
+	}
+	void readConectedPlayerLevel(std::queue<uint8_t>& gameData)
+	{
+		level = convertQueueToType<uint32_t>(gameData);
+		gameData.pop();
+	}
 
-            uint32_t addrsGuidNone = hobbitProcessAnalyzer->findGameObjByGUID(guidNone);
-            if (addrsGuidNone)
-                npc.setWeapon(hobbitProcessAnalyzer->readData<uint32_t>(addrsGuidNone + 0x260));
-        }
-       
+	void processPlayer(uint8_t myId)
+	{
+		if (id == -1 || id == myId)
+			return;
 
+		// Display the data
+		logOption_->LogMessage(LogLevel::Log_Debug, "Process Msg");
+		logOption_->increaseDepth();
+		logOption_->LogMessage(LogLevel::Log_Debug, "Anim", animation);
+		logOption_->LogMessage(LogLevel::Log_Debug, "Pos", position.x, position.y, position.z);
+		logOption_->LogMessage(LogLevel::Log_Debug, "RotY", rotation.y);
+		logOption_->LogMessage(LogLevel::Log_Debug, "Weapon", int(weapon));
+		logOption_->decreaseDepth();
 
-        // Update Health of Enemies
-        while (!hurtEnemies.empty())
-        {
-            if (hurtEnemies.front().first == 0)
-            {
-                hurtEnemies.pop();
-                continue;
-            }
-            logOption_->LogMessage(LogLevel::Log_Debug, "Enemy Hurt");
-            logOption_->increaseDepth();
-            logOption_->LogMessage(LogLevel::Log_Debug, "GUID:", hurtEnemies.front().first, "New Health:", hurtEnemies.front().second);
-            logOption_->decreaseDepth();
-
-            //find by guid
-            uint32_t objAddrs = hobbitProcessAnalyzer->findGameObjByGUID(hurtEnemies.front().first);
-
-            //check if the object exist
-            if (objAddrs != 0)
-            {
-                float health = hobbitProcessAnalyzer->readData<float>(objAddrs + 0x290);
-                if (health > hurtEnemies.front().second)
-                {
-                    hobbitProcessAnalyzer->writeData<float>(objAddrs + 0x290, hurtEnemies.front().second);
-                    hurtEnemies.pop();
-                    continue;
-                }
-            }
-            hurtEnemies.pop();
-
-        }
-
-       
-    }
+		//set position, rotation, and animation
+		npc.setPositionX(position.x);
+		npc.setPositionY(position.y);
+		npc.setPositionZ(position.z);
+		npc.setRotationY(rotation.y);
+		if (animation != npc.getAnimation())
+		{
+			npc.setAnimation(animation);
+			npc.setAnimFrames(animFrame, lastAnimFrame);
+		}
 
 
-    std::vector<BaseMessage> write() {
-        //In case you need to send some state from the npc bilbo (which is unlickely) 
-        return std::vector<BaseMessage>();
-    }
+		uint64_t guidNone = 0x0;
+		if (weapon != -1)
+		{
+			//NONE
+			if (weapon == 2)
+				guidNone = 0x0D8AD910E885100D;
+			//STING
+			else if (weapon == 0)
+				guidNone = 0x0D8AD910E885100B;
+			//STAFF
+			else if (weapon == 1)
+				guidNone = 0x0D8AD910E885100A;
+			//STONE
+			else if (weapon == 3)
+				guidNone = 0x0D8AD910E885100C;
 
-    void clear() { id = -1; }
+			uint32_t addrsGuidNone = hobbitProcessAnalyzer->findGameObjByGUID(guidNone);
+			if (addrsGuidNone)
+				npc.setWeapon(hobbitProcessAnalyzer->readData<uint32_t>(addrsGuidNone + 0x260));
+		}
+
+
+
+		// Update Health of Enemies
+		while (!hurtEnemies.empty())
+		{
+			if (hurtEnemies.front().first == 0)
+			{
+				hurtEnemies.pop();
+				continue;
+			}
+			logOption_->LogMessage(LogLevel::Log_Debug, "Enemy Hurt");
+			logOption_->increaseDepth();
+			logOption_->LogMessage(LogLevel::Log_Debug, "GUID:", hurtEnemies.front().first, "New Health:", hurtEnemies.front().second);
+			logOption_->decreaseDepth();
+
+			//find by guid
+			uint32_t objAddrs = hobbitProcessAnalyzer->findGameObjByGUID(hurtEnemies.front().first);
+
+			//check if the object exist
+			if (objAddrs != 0)
+			{
+				float health = hobbitProcessAnalyzer->readData<float>(objAddrs + 0x290);
+				if (health > hurtEnemies.front().second)
+				{
+					hobbitProcessAnalyzer->writeData<float>(objAddrs + 0x290, hurtEnemies.front().second);
+					hurtEnemies.pop();
+					continue;
+				}
+			}
+			hurtEnemies.pop();
+
+		}
+
+
+	}
+
+
+	std::vector<BaseMessage> write() {
+		//In case you need to send some state from the npc bilbo (which is unlickely) 
+		return std::vector<BaseMessage>();
+	}
+
+	void clear() { id = -1; }
 };
--- Hobbit Multiplayer/HobbitClient.h
+++ Hobbit Multiplayer/HobbitClient.h
@@ -29,300 +29,318 @@
 // Main client class
 class HobbitClient {
 public:
-    HobbitClient(std::string initialServerIp = "")
-        : serverIp(std::move(initialServerIp)), running(false), processMessages(false), logOption_(LogManager::Instance().CreateLogOption("HOBBIT CLIENT")) {
-        for (auto& e :  connectedPlayers)
-        {
-            e.setHobbitProcessAnalyzer(hobbitGameManager);
-        }
-        mainPlayer.setHobbitProcessAnalyzer(hobbitGameManager);
-    }
-    ~HobbitClient() { stop(); }
+	HobbitClient(std::string initialServerIp = "")
+		: serverIp(std::move(initialServerIp)), running(false), processMessages(false), logOption_(LogManager::Instance().CreateLogOption("HOBBIT CLIENT")) {
+		for (auto& e : connectedPlayers)
+		{
+			e.setHobbitProcessAnalyzer(hobbitGameManager);
+		}
+		mainPlayer.setHobbitProcessAnalyzer(hobbitGameManager);
+	}
+	~HobbitClient() { stop(); }
 
-    int start();
-    int start(const std::string& ip);
-    void stop();
+	int start();
+	int start(const std::string& ip);
+	void stop();
 
-    bool isRunning() { return running; }
+	bool isRunning() { return running; }
 
 private:
-    LogOption::Ptr logOption_;
-    Client client;
-    HobbitGameManager hobbitGameManager;
+	LogOption::Ptr logOption_;
+	Client client;
+	HobbitGameManager hobbitGameManager;
 
-    std::vector<uint64_t> guids;
+	std::vector<uint64_t> guids;
 
-    std::string serverIp;
-
-
-    std::thread updateThread;
-    std::atomic<bool> running;
-    std::atomic<bool> processMessages = false;
+	std::string serverIp;
 
 
-    static constexpr int MAX_PLAYERS = 7;
-    ConnectedPlayer connectedPlayers[MAX_PLAYERS];
-    MainPlayer mainPlayer;
+	std::thread updateThread;
+	std::atomic<bool> running;
+	std::atomic<bool> processMessages = false;
 
-    void update();
-    void readMessage();
-    void readGameMessage(int senderID, std::queue<uint8_t>& gameData);
-    void writeMessage();
 
-    void onEnterNewLevel();
-    void onExitLevel() { processMessages = false; }
+	static constexpr int MAX_PLAYERS = 7;
+	ConnectedPlayer connectedPlayers[MAX_PLAYERS];
+	MainPlayer mainPlayer;
 
-    void onOpenGame();
-    void onCloseGame() { processMessages = false; stop(); }
+	void update();
+	void readMessage();
+	void readGameMessage(int senderID, std::queue<uint8_t>& gameData);
+	void writeMessage();
 
-    void onClientListUpdate(const std::queue<uint8_t>&);
+	void onEnterNewLevel();
+	void onExitLevel() { processMessages = false; }
 
-    std::vector<uint64_t> getPlayersNpcGuid();
+	void onOpenGame();
+	void onCloseGame() { processMessages = false; stop(); }
+
+	void onClientListUpdate(const std::queue<uint8_t>&);
+
+	std::vector<uint64_t> getPlayersNpcGuid();
 };
 
 int HobbitClient::start() {
-    logOption_->LogMessage(LogLevel::Log_Prompt, "Enter server IP address: ");
+	std::ifstream file("./server_ip.txt");
+	if (file) {
+		// Read the first line from server_ip.txt if it exists
+		std::getline(file, serverIp);
 
-    std::cin >> serverIp;
-    std::cin.ignore();
+		if (!serverIp.empty()) {
+			logOption_->LogMessage(LogLevel::Log_Prompt, "Using server IP from file: " + serverIp);
+		}
+		else {
+			// File is empty, proceed with user input
+			logOption_->LogMessage(LogLevel::Log_Prompt, "server_ip.txt is empty. Enter server IP address: ");
+			std::cin >> serverIp;
+			std::cin.ignore();
+		}
+	}
+	else {
+		// File doesn't exist, prompt for IP address
+		logOption_->LogMessage(LogLevel::Log_Prompt, "Enter server IP address: ");
+		std::cin >> serverIp;
+		std::cin.ignore();
+	}
 
-    return start(serverIp);
+	return start(serverIp);
 }
+
 int HobbitClient::start(const std::string& ip) {
-    serverIp = ip;
+	serverIp = ip;
 
-    client.addListener([this](const std::queue<uint8_t>& clientIDs) {
-        onClientListUpdate(clientIDs);
-        });
+	client.addListener([this](const std::queue<uint8_t>& clientIDs) {
+		onClientListUpdate(clientIDs);
+		});
 
-    if (client.start(serverIp)) return 1;
+	if (client.start(serverIp)) return 1;
 
-    guids = getPlayersNpcGuid();
+	guids = getPlayersNpcGuid();
 
-    while (!hobbitGameManager.isGameRunning()) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        logOption_->LogMessage(LogLevel::Log_Prompt, "You must open The Hobbit 2003 game!");
-    }
+	while (!hobbitGameManager.isGameRunning()) {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		logOption_->LogMessage(LogLevel::Log_Prompt, "You must open The Hobbit 2003 game!");
+	}
 
-    running = true;
-    hobbitGameManager.addListenerEnterNewLevel([this] { onEnterNewLevel(); });
-    hobbitGameManager.addListenerExitLevel([this] { onExitLevel(); });
-    hobbitGameManager.addListenerOpenGame([this] { onOpenGame(); });
-    hobbitGameManager.addListenerCloseGame([this] { onCloseGame(); });
+	running = true;
+	hobbitGameManager.addListenerEnterNewLevel([this] { onEnterNewLevel(); });
+	hobbitGameManager.addListenerExitLevel([this] { onExitLevel(); });
+	hobbitGameManager.addListenerOpenGame([this] { onOpenGame(); });
+	hobbitGameManager.addListenerCloseGame([this] { onCloseGame(); });
 
-    hobbitGameManager.start();
+	hobbitGameManager.start();
 
-    onOpenGame();
+	onOpenGame();
 
-    updateThread = std::thread(&HobbitClient::update, this);
-    return 0;
+	updateThread = std::thread(&HobbitClient::update, this);
+	return 0;
 
 }
 
 void HobbitClient::stop() {
-    running = false;
-    client.stop();
-    hobbitGameManager.stop();
+	running = false;
+	client.stop();
+	hobbitGameManager.stop();
 
-    if (updateThread.joinable()) {
-        updateThread.join();
-    }
+	if (updateThread.joinable()) {
+		updateThread.join();
+	}
 }
 void HobbitClient::update() {
-    while (running) {
-        if (processMessages)
-        {
-            if (!hobbitGameManager.isOnLevel())
-            {
-                std::this_thread::sleep_for(std::chrono::milliseconds(500));
-                continue;
-            }
-        }
-        else
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            continue;
-        }
-        readMessage();
-        for (int i = 0; i < MAX_PLAYERS; ++i)
-        {
-            connectedPlayers[i].processPlayer(client.getClientID());
-        }
-        writeMessage();
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    }
+	while (running) {
+		if (processMessages)
+		{
+			if (!hobbitGameManager.isOnLevel())
+			{
+				std::this_thread::sleep_for(std::chrono::milliseconds(500));
+				continue;
+			}
+		}
+		else
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+			continue;
+		}
+		readMessage();
+		for (int i = 0; i < MAX_PLAYERS; ++i)
+		{
+			connectedPlayers[i].processPlayer(client.getClientID());
+		}
+		writeMessage();
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	}
 }
 
 void HobbitClient::readMessage() {
 
-    BaseMessage textMessageOpt = client.frontTextMessage();
-    if (textMessageOpt.message.size() > 0) {
-        // Assuming textMessageOpt.message is a queue of characters or strings
-        std::string fullMessage;
-        while (!textMessageOpt.message.empty()) {
-            fullMessage += textMessageOpt.message.front(); // Get the front message
-            textMessageOpt.message.pop(); // Remove the front message from the queue
-        }
+	BaseMessage textMessageOpt = client.frontTextMessage();
+	if (textMessageOpt.message.size() > 0) {
+		// Assuming textMessageOpt.message is a queue of characters or strings
+		std::string fullMessage;
+		while (!textMessageOpt.message.empty()) {
+			fullMessage += textMessageOpt.message.front(); // Get the front message
+			textMessageOpt.message.pop(); // Remove the front message from the queue
+		}
 
-        logOption_->LogMessage(LogLevel::Log_Debug, "Received Text Message from", int(textMessageOpt.senderID), ":", fullMessage);
+		logOption_->LogMessage(LogLevel::Log_Debug, "Received Text Message from", int(textMessageOpt.senderID), ":", fullMessage);
 
-        client.popFrontTextMessage();
-    }
+		client.popFrontTextMessage();
+	}
 
-    BaseMessage eventMessageOpt = client.frontEventMessage();
-    if (eventMessageOpt.message.size() > 0) {
-        readGameMessage(eventMessageOpt.senderID, eventMessageOpt.message);
-        client.popFrontEventMessage();
-    }
+	BaseMessage eventMessageOpt = client.frontEventMessage();
+	if (eventMessageOpt.message.size() > 0) {
+		readGameMessage(eventMessageOpt.senderID, eventMessageOpt.message);
+		client.popFrontEventMessage();
+	}
 
-    std::map<uint8_t, BaseMessage> snapshotMessages = client.snapMessage();
-    for (auto& pair : snapshotMessages) {
-        readGameMessage(pair.first, pair.second.message);
-        client.clearSnapMessage();
-    }
+	std::map<uint8_t, BaseMessage> snapshotMessages = client.snapMessage();
+	for (auto& pair : snapshotMessages) {
+		readGameMessage(pair.first, pair.second.message);
+		client.clearSnapMessage();
+	}
 
 }
 void HobbitClient::writeMessage() {
 
-    std::vector<BaseMessage> messages;
+	std::vector<BaseMessage> messages;
 
-    std::vector<BaseMessage> mainPlayerMsg = mainPlayer.write();
-    std::vector<BaseMessage> connectedPlayerMsg = mainPlayer.write();
+	std::vector<BaseMessage> mainPlayerMsg = mainPlayer.write();
+	std::vector<BaseMessage> connectedPlayerMsg = mainPlayer.write();
 
-    messages.insert(messages.end(), mainPlayerMsg.begin(), mainPlayerMsg.end());
-    messages.insert(messages.end(), connectedPlayerMsg.begin(), connectedPlayerMsg.end());
+	messages.insert(messages.end(), mainPlayerMsg.begin(), mainPlayerMsg.end());
+	messages.insert(messages.end(), connectedPlayerMsg.begin(), connectedPlayerMsg.end());
 
-    for (BaseMessage& e : messages)
-    {
-        e.senderID = client.getClientID();
+	for (BaseMessage& e : messages)
+	{
+		e.senderID = client.getClientID();
 
-        if (e.message.size() > 0)
-            client.sendMessage(e);
-    }
+		if (e.message.size() > 0)
+			client.sendMessage(e);
+	}
 }
 void HobbitClient::readGameMessage(int senderID, std::queue<uint8_t>& gameData) {
-    logOption_->LogMessage(LogLevel::Log_Debug, "Received Game Massage from", senderID);
+	logOption_->LogMessage(LogLevel::Log_Debug, "Received Game Massage from", senderID);
 
-    while (!gameData.empty()) {
-        DataLabel label = static_cast<DataLabel>(gameData.front());
-        gameData.pop();
+	while (!gameData.empty()) {
+		DataLabel label = static_cast<DataLabel>(gameData.front());
+		gameData.pop();
 
-        uint8_t size_tmp = gameData.front();
-        gameData.pop();
+		uint8_t size_tmp = gameData.front();
+		gameData.pop();
 
-        if (label == DataLabel::CONNECTED_PLAYER_SNAP)
-        {
-            auto it = std::find_if(std::begin(connectedPlayers), std::end(connectedPlayers),
-                [&](const ConnectedPlayer& p) { return p.id == senderID; });
-            if (it != std::end(connectedPlayers)) {
-                it->readConectedPlayerSnap(gameData);
-            }
-            else {
-                logOption_->LogMessage(LogLevel::Log_Error, "Unregistered player id", senderID);
-                connectedPlayers[0].readConectedPlayerSnap(gameData);
-            }
+		if (label == DataLabel::CONNECTED_PLAYER_SNAP)
+		{
+			auto it = std::find_if(std::begin(connectedPlayers), std::end(connectedPlayers),
+				[&](const ConnectedPlayer& p) { return p.id == senderID; });
+			if (it != std::end(connectedPlayers)) {
+				it->readConectedPlayerSnap(gameData);
+			}
+			else {
+				logOption_->LogMessage(LogLevel::Log_Error, "Unregistered player id", senderID);
+				connectedPlayers[0].readConectedPlayerSnap(gameData);
+			}
 
-        }
-        else if (label == DataLabel::ENEMIES_HEALTH)
-        {
-            auto it = std::find_if(std::begin(connectedPlayers), std::end(connectedPlayers),
-                [&](const ConnectedPlayer& p) { return p.id == senderID; });
-            if (it != std::end(connectedPlayers)) {
-                it->readProcessEnemiesHealth(gameData);
-            }
-            else {
-                logOption_->LogMessage(LogLevel::Log_Error, "Unregistered player id", senderID);
-                connectedPlayers[0].readProcessEnemiesHealth(gameData);
-            }
-        }
-        else if (label == DataLabel::INVENTORY)
-        {
-            mainPlayer.readProcessInventory(gameData);
-        }
-        else
-        {
-            logOption_->LogMessage(LogLevel::Log_Error, "Unknown label received", int(label));
-        }
-    }
+		}
+		else if (label == DataLabel::ENEMIES_HEALTH)
+		{
+			auto it = std::find_if(std::begin(connectedPlayers), std::end(connectedPlayers),
+				[&](const ConnectedPlayer& p) { return p.id == senderID; });
+			if (it != std::end(connectedPlayers)) {
+				it->readProcessEnemiesHealth(gameData);
+			}
+			else {
+				logOption_->LogMessage(LogLevel::Log_Error, "Unregistered player id", senderID);
+				connectedPlayers[0].readProcessEnemiesHealth(gameData);
+			}
+		}
+		else if (label == DataLabel::INVENTORY)
+		{
+			mainPlayer.readProcessInventory(gameData);
+		}
+		else
+		{
+			logOption_->LogMessage(LogLevel::Log_Error, "Unknown label received", int(label));
+		}
+	}
 }
 
 
 void HobbitClient::onEnterNewLevel() {
 
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+	std::this_thread::sleep_for(std::chrono::seconds(5));
 
-    hobbitGameManager.getHobbitProcessAnalyzer()->updateObjectStackAddress();
-    
-    
-    if (guids.size() == 0)
-        guids = getPlayersNpcGuid();
+	hobbitGameManager.getHobbitProcessAnalyzer()->updateObjectStackAddress();
 
-    for (int i = 0; i < MAX_PLAYERS; ++i)
-    {
-        connectedPlayers[i].npc.setNCP(guids[i]);
-    }
 
-    mainPlayer.readPtrs();
+	if (guids.size() == 0)
+		guids = getPlayersNpcGuid();
 
-    processMessages = true;
+	for (int i = 0; i < MAX_PLAYERS; ++i)
+	{
+		connectedPlayers[i].npc.setNCP(guids[i]);
+	}
+
+	mainPlayer.readPtrs();
+
+	processMessages = true;
 }
 void HobbitClient::onOpenGame()
 {
-    processMessages = false;
+	processMessages = false;
 
-    if (hobbitGameManager.isOnLevel())
-        onEnterNewLevel();
+	if (hobbitGameManager.isOnLevel())
+		onEnterNewLevel();
 }
 
 
 void HobbitClient::onClientListUpdate(const std::queue<uint8_t>&) {
-    std::queue<uint8_t> connectedClients = client.getConnectedClients();
-    running = (connectedClients.size() != 0);
+	std::queue<uint8_t> connectedClients = client.getConnectedClients();
+	running = (connectedClients.size() != 0);
 
-    for (int i = 0; i < MAX_PLAYERS; ++i) {
-        connectedPlayers[i].id = connectedClients.empty() ? -1 : connectedClients.front();
-        if (!connectedClients.empty()) connectedClients.pop(); // Remove the front element from the queue
-    }
+	for (int i = 0; i < MAX_PLAYERS; ++i) {
+		connectedPlayers[i].id = connectedClients.empty() ? -1 : connectedClients.front();
+		if (!connectedClients.empty()) connectedClients.pop(); // Remove the front element from the queue
+	}
 
 }
 std::vector<uint64_t> HobbitClient::getPlayersNpcGuid() {
-    std::ifstream file;
-    std::string filePath = "FAKE_BILBO_GUID.txt";
-    bool foudFileInitially = true;;
-    // Open the file, prompting the user if it doesn't exist
-    while (!file.is_open()) {
-        file.open(filePath);
-        if (!file.is_open()) {
-            foudFileInitially = false;
-            logOption_->LogMessage(LogLevel::Log_Prompt, "File not found. Enter the path to FAKE_BILBO_GUID.txt or 'q' to quit: ");
-            std::string input;
-            std::getline(std::cin, input);
-            if (input == "q") return {}; // Quit if user enters 'q'
-            filePath = input;
-        }
-    }
+	std::ifstream file;
+	std::string filePath = "FAKE_BILBO_GUID.txt";
+	bool foudFileInitially = true;;
+	// Open the file, prompting the user if it doesn't exist
+	while (!file.is_open()) {
+		file.open(filePath);
+		if (!file.is_open()) {
+			foudFileInitially = false;
+			logOption_->LogMessage(LogLevel::Log_Prompt, "File not found. Enter the path to FAKE_BILBO_GUID.txt or 'q' to quit: ");
+			std::string input;
+			std::getline(std::cin, input);
+			if (input == "q") return {}; // Quit if user enters 'q'
+			filePath = input;
+		}
+	}
 
-    std::vector<uint64_t> tempGUID;
-    std::string line;
+	std::vector<uint64_t> tempGUID;
+	std::string line;
 
-    // Read each line from the file
-    while (std::getline(file, line)) {
-        // Find the position of the underscore
-        size_t underscorePos = line.find('_');
-        if (underscorePos != std::string::npos) {
-            // Split the line into two parts
-            std::string part2 = line.substr(0, underscorePos); // Before the underscore
-            std::string part1 = line.substr(underscorePos + 1); // After the underscore
+	// Read each line from the file
+	while (std::getline(file, line)) {
+		// Find the position of the underscore
+		size_t underscorePos = line.find('_');
+		if (underscorePos != std::string::npos) {
+			// Split the line into two parts
+			std::string part2 = line.substr(0, underscorePos); // Before the underscore
+			std::string part1 = line.substr(underscorePos + 1); // After the underscore
 
-            // Concatenate the parts in reverse order
-            std::string combined = part2 + part1;
+			// Concatenate the parts in reverse order
+			std::string combined = part2 + part1;
 
-            // Convert the combined string to uint64_t
-            uint64_t guid = std::stoull(combined, nullptr, 16); // Convert from hex string to uint64_t
-            tempGUID.push_back(guid);
-        }
-    }
-    if(foudFileInitially)
-        logOption_->LogMessage(LogLevel::Log_Info, "FOUND FILE!");
-    return tempGUID;
+			// Convert the combined string to uint64_t
+			uint64_t guid = std::stoull(combined, nullptr, 16); // Convert from hex string to uint64_t
+			tempGUID.push_back(guid);
+		}
+	}
+	if (foudFileInitially)
+		logOption_->LogMessage(LogLevel::Log_Info, "FOUND FILE!");
+	return tempGUID;
 }
